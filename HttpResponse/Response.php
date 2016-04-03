426cb94727444bbf0317b17a27a6525c3939a6a6
Adriano Rosa <info@adrianorosa.com>
2016-04-03 00:40:42 -0300

    import sources

---
+++ HttpResponse/Response.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace AdrianoRosa\HttpResponse;
+
+use \Illuminate\Http\Response as BaseResponse;
+
+/**
+ * Class Response
+ *
+ * @author  Adriano Rosa <http://adrianorosa.com>
+ * @date    02/04/16 14:51
+ *
+ * @package Http
+ */
+class Response extends BaseResponse
+{
+    /**
+     * Format a safe JSON Response.
+     *
+     * @param array $data
+     * @param int $code
+     * @param array $headers
+     *
+     * @return $this
+     */
+    public function safeJson($data = [], $code = null, $headers = [])
+    {
+        if ( ! is_null($code) ) {
+            $this->statusCode = $code;
+        }
+
+        if ( empty($data) ) {
+            $data = $this->content ?: [];
+        }
+
+        if ( $data instanceof \Illuminate\Support\Collection
+            || $data instanceof \Illuminate\Database\Eloquent\Model
+        ) {
+            $data = $data->toArray();
+        }
+
+        $status = ($this->statusCode >= 100 && $this->statusCode <= 308) ? 'success' : 'error';
+
+        if ( is_string($data) ) {
+            $data = (array) $data;
+        }
+
+        $content = $this->morphToJson(
+            (new ResponseFormatter($data, $this->statusCode, $status))
+                ->getResponse()
+        );
+
+        $safeResponse = ")]}',\n" . $content;
+
+        $default = [
+            'Content-Type' => 'application/json'
+        ];
+
+        return $this->setContent($safeResponse)->withHeaders(array_merge($default, $headers));
+    }
+
+    /**
+     * Factory method for chainability.
+     *
+     * Example:
+     *
+     *     return Response::create($body, 200)
+     *         ->setSharedMaxAge(300);
+     *
+     * @param mixed $content The response content, see setContent()
+     * @param int   $status  The response status code
+     * @param array $headers An array of response headers
+     *
+     * @return Response
+     */
+    public static function create($content = '', $status = 200, $headers = array())
+    {
+        return new static($content, $status, $headers);
+    }
+}
---
+++ HttpResponse/ResponseFormatter.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace AdrianoRosa\HttpResponse;
+
+class ResponseFormatter
+{
+
+    /**
+     * @type array
+     */
+    protected $response = [
+        'code' => 404,
+        'status' => 'Not Found',
+        'data' => [],
+    ];
+
+    /**
+     * @param array $data
+     * @param int $code
+     * @param string $status
+     */
+    public function __construct(array $data = [], $code = 200, $status = 'success')
+    {
+        $this->setFormatResponse($data, $code, $status);
+    }
+
+    /**
+     * @param        $data
+     * @param int    $code
+     * @param string $status
+     *
+     * @return $this
+     */
+    public function setFormatResponse(array $data = [], $code = 200, $status = 'success')
+    {
+        $this->response['code']   = $code;
+        $this->response['status'] = $status;
+        $this->response['data']   = $data;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $key
+     * @param mixed $value
+     *
+     * @deprecated
+     * @return $this
+     */
+    public function setAppendToResponse($key, $value = null)
+    {
+        if (is_array($key)) {
+            foreach ($key as $k => $v) {
+                $this->setAppendToResponse($k, $v);
+            }
+            return $this;
+        }
+
+        $this->response[$key] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get the response data formatted
+     *
+     * @return array
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+    /**
+     * Get the response data formatted
+     *
+     * @deprecated
+     * @return array
+     */
+    public function getResponseData()
+    {
+        return $this->response['data'];
+    }
+
+    /**
+     * @deprecated
+     * @return mixed
+     */
+    public function getStatusCode()
+    {
+        return $this->response['code'];
+    }
+
+    /**
+     * @deprecated
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->response['status'];
+    }
+}
